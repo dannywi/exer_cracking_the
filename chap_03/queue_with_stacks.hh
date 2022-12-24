2832b3faa9201ded40e23be95d8bc7ebc466840b
Danny W <54228968+dannywi@users.noreply.github.com>
2022-12-24 16:40:11 +0900

    Add stack based sorting

--- chap_03/queue_with_stacks.hh
+++ chap_03/queue_with_stacks.hh
@@ -1,36 +1,39 @@
 #pragma once
 
+#include <iostream>
 #include <vector>
+
+// rudimentary stack
+template <typename T>
+class stack {
+  std::vector<T> vals_;
+  void throw_on_empty() const {
+    if (vals_.empty()) {
+      throw std::logic_error("empty");
+    }
+  }
+
+ public:
+  void push(T v) { vals_.push_back(v); }
+
+  T peek() const {
+    throw_on_empty();
+    return vals_.back();
+  }
+
+  void pop() {
+    throw_on_empty();
+    vals_.pop_back();
+  }
+
+  bool empty() const { return vals_.empty(); }
+};
 
 // implement queue using stacks
 template <typename T>
 class queue_with_stacks {
-  class stack {
-    std::vector<T> vals_;
-    void throw_on_empty() const {
-      if (vals_.empty()) {
-        throw std::logic_error("empty");
-      }
-    }
-
-   public:
-    void push(T v) { vals_.push_back(v); }
-
-    T peek() const {
-      throw_on_empty();
-      return vals_.back();
-    }
-
-    void pop() {
-      throw_on_empty();
-      vals_.pop_back();
-    }
-
-    bool empty() const { return vals_.empty(); }
-  };
-
-  stack in_list;
-  stack out_list;
+  stack<T> in_list;
+  stack<T> out_list;
 
   void shift_to_out() {
     // this is an optimization, instead shifting everything to in_list before push
@@ -64,3 +67,49 @@ class queue_with_stacks {
 
   bool empty() const { return out_list.empty() && in_list.empty(); }
 };
+
+namespace util {
+template <typename T, typename COMP>
+void sort_with_stacks(queue_with_stacks<T>& queue, COMP comp) {
+  stack<T> tgt, keep;
+  while (!queue.empty()) {
+    // 1. pop from queue
+    T v = queue.peek();
+    queue.pop();
+
+    // 2. while tgt not empty and comp tgt top not true, move to keep
+    while (!tgt.empty() && !comp(v, tgt.peek())) {
+      T t = tgt.peek();
+      tgt.pop();
+      keep.push(t);
+    }
+
+    // 3. insert val
+    tgt.push(v);
+
+    // 4. move back keep to tgt
+    while (!keep.empty()) {
+      T k = keep.peek();
+      keep.pop();
+      tgt.push(k);
+    }
+  }
+
+  // 5. insert tgt to queue
+  while (!tgt.empty()) {
+    T v = tgt.peek();
+    tgt.pop();
+    queue.push(v);
+  }
+}
+
+template <typename T>
+void flush_queue(queue_with_stacks<T>& q) {
+  std::cout << "[ ";
+  while (!q.empty()) {
+    std::cout << q.peek() << " ";
+    q.pop();
+  }
+  std::cout << "]" << std::endl;
+}
+}  // namespace util
--- chap_03/test_runner.cc
+++ chap_03/test_runner.cc
@@ -1,5 +1,8 @@
 #include <gtest/gtest.h>
 
+#include <cstdlib>
+#include <ctime>
+
 #include "queue_with_stacks.hh"
 
 TEST(queue_with_stacks, BasicAssertions) {
@@ -26,4 +29,22 @@ TEST(queue_with_stacks, BasicAssertions) {
   EXPECT_EQ(queue.empty(), true);
   EXPECT_THROW(queue.peek(), std::logic_error);
   EXPECT_THROW(queue.pop(), std::logic_error);
+
+  auto check_sorted = [](auto cont, auto comp) -> bool {
+    while (!cont.empty()) {
+      auto v = cont.peek();
+      cont.pop();
+      if (cont.empty()) break;
+      if (!comp(v, cont.peek())) return false;
+    }
+    return true;
+  };
+
+  std::srand(std::time(nullptr));
+  for (size_t i = 0; i < 10; ++i) queue.push(std::rand() % 50 - 25);
+
+  auto comp = [](auto a, auto b) -> bool { return a < b; };
+  util::sort_with_stacks(queue, comp);
+  EXPECT_TRUE(check_sorted(queue, comp));
+  util::flush_queue(queue);
 }
